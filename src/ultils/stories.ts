a74f93ed94145759042cb90dfb0d0a851f673874
ZackCgl <100427794+ZackCgl@users.noreply.github.com>
2023-05-29 00:07:35 +0200

    feature: rem title projects. add broky.dev project

--- src/ultils/stories.ts
+++ src/ultils/stories.ts
@@ -13,87 +13,6 @@ export const blogs = [
     },
   ];
 
-  export const projects = [
-    {
-      id: 1,
-      title: "Ruby Finance",
-      category: "Website",
-      image: "/websites/ruby.jpg",
-    },
-    {
-      id: 2,
-      title: "Online with you",
-      category: "website",
-      image: "/websites/owy.jpg",
-    },
-    {
-      id: 3,
-      title: "Ruby App",
-      category: "Web-app",
-      image: "/websites/rubyapp.png",
-    },
-    {
-      id: 4,
-      title: "Veta Ent.",
-      category: "Website",
-      image: "/websites/vetawebsite.jpg",
-    },
-    {
-      id: 5,
-      title: "Veta Ent.",
-      category: "web-app",
-      image: "/websites/vetaapp.jpg",
-    },
-    {
-      id: 6,
-      title: "Dead habits",
-      category: "Blockchain Web-app",
-      image: "/websites/deadhabitswebsite.jpg",
-    },
-    {
-      id: 7,
-      title: "Brand Ninja",
-      category: " Website",
-      image: "/websites/brandninjawebsite.png",
-    },
-    {
-      id: 8,
-      title: "Crew stars",
-      category: "Website",
-      image: "/websites/crewstarswebsite.jpg",
-    },
-    {
-      id: 9,
-      title: "Iron Stove",
-      category: "Website",
-      image: "/websites/ironstovewebsite.jpg",
-    },
-    {
-      id: 10,
-      title: "Mark & ting",
-      category: "Website",
-      image: "/websites/markentingwebsite.jpg",
-    },
-    {
-      id: 11,
-      title: "Kippie kasstaat",
-      category: "Web-app",
-      image: "/websites/kasstaatapp.png",
-    },
-    {
-      id: 12,
-      title: "Tsx Tech",
-      category: "Website",
-      image: "/websites/tsxwebsite.png",
-    },
-    {
-      id: 13,
-      title: "Meta-buddies",
-      category: "Website",
-      image: "/websites/metabuddieswebsite.jpg",
-    },
-  ];
-
   export const projects1 = [
     {
       id: 1,
@@ -191,6 +110,12 @@ export const blogs = [
       title: "Ultra Cura",
       category: "Website & admin panel",
       image: "/websites/ultracura.png",
+    },
+    {
+      id: 15,
+      title: "broky.dev",
+      category: "Website",
+      image: "/websites/brokyweb.jpg",
     },
     
   ];
