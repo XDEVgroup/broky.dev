7322a605a20407280038018f8987bd339023b30c
markjkaem <mark@rubyfinance.nl>
2023-11-18 20:16:42 +0100

    feat: add animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,29 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const store = useStore({
+    scrolled: false,
+  });
   return (
     <header>
-      <div class="fixed top-0 z-20 h-20 w-screen text-white ">
-        <div class="flex justify-between items-center md:p-5 p-4 md:px-20 px-6">
+      <div class="fixed top-0 z-20 h-20 w-screen text-white bg-transparent backdrop-blur-sm">
+        <div  class={
+          store.scrolled
+            ? "flex justify-between items-center md:p-5 p-4 md:px-20 px-6"
+            : "flex justify-between items-center md:p-5 p-4 md:px-40 px-10"
+        }
+        document:onScroll$={() => {
+          if (window.scrollY > 0) {
+            store.scrolled = true;
+          } else {
+            store.scrolled = false;
+          }
+        }} >
           <div>
             <Link
-              class="font-semibold md:text-3xl text-2xl"
+            class={store.scrolled ? "font-semibold md:text-3xl text-2xl" : "font-semibold md:text-4xl text-3xl"}
+            
               href="/"
               title="qwik"
             >
@@ -18,7 +33,7 @@ export default component$(() => {
           <ul class="flex space-x-6">
             <li>
               <Link
-                class="md:text-xl text-lg hover:text-gray-300 transition"
+              class={store.scrolled ? "md:text-xl text-lg hover:text-gray-300 transition" : "md:text-2xl text-2xl hover:text-gray-300 transition"}
                 href="/cases"
               >
                 Portfolio
--- src/components/headercontent.tsx
+++ src/components/headercontent.tsx
@@ -1,11 +1,25 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const store = useStore({
+    scrolled: false,
+  });
   return (
     <header>
       <div class="fixed top-0 z-20 h-20 w-screen text-black bg-transparent backdrop-blur-sm">
-        <div class="flex justify-between items-center md:p-5 p-4 md:px-20 px-6">
+        <div  class={
+          store.scrolled
+            ? "flex justify-between items-center md:p-5 p-4 md:px-20 px-6"
+            : "flex justify-center items-center md:p-5 p-4 md:px-40 px-14"
+        }
+        document:onScroll$={() => {
+          if (window.scrollY > 0) {
+            store.scrolled = true;
+          } else {
+            store.scrolled = false;
+          }
+        }} >
           <div>
             <Link
               class="font-semibold md:text-3xl text-2xl"
