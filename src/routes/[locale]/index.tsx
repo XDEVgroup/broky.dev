e4312c38ede6c4f0a5f52e366a8b8c15fd97fbb2
markjkaem <mark@rubyfinance.nl>
2023-11-21 23:43:55 +0100

    feat: removed contact page

--- src/routes/[locale]/index.tsx
+++ src/routes/[locale]/index.tsx
@@ -96,7 +96,7 @@ export default component$(() => {
               <Link
                 target="_blank"
                 class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 dark:focus:ring-offset-gray-800"
-                href="/en/contact"
+                href="https://www.linkedin.com/in/markteekens/"
               >
                 {$localize`:@@hireme:`}
               </Link>
