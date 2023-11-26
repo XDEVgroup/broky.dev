bf050b60596eefdaf587c26a10811dc8b0456cc3
markjkaem <mark@rubyfinance.nl>
2023-11-26 15:01:35 +0100

    fet add ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import { vercelEdgeAdapter } from '@builder.io/qwik-city/adapters/vercel-edge/vite';
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
 export default defineConfig(() => {
   return {
-    build: {
-      ssr: false,
-    },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), vercelEdgeAdapter({
-     
-      ssg: {
-        include: ['/*'],
-        origin: 'https://broky.dev',
-        sitemapOutFile: 'sitemap.xml',
-      },
-    })],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
