633a5fa2f4b5797d3965c47c0fad2d6db0565f15
markjkaem <mark@rubyfinance.nl>
2023-11-26 14:53:10 +0100

    add outputconfig

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths(), vercelEdgeAdapter({
+      outputConfig: true,
       ssg: {
         include: ['/*'],
         origin: 'https://broky.dev',
