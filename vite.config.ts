93ba7a3210d0eb81c2fc5d13aca87ee7e340571b
markjkaem <mark@rubyfinance.nl>
2023-11-26 14:43:19 +0100

    feat: add vercel sitemap adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { vercelEdgeAdapter } from '@builder.io/qwik-city/adapters/vercel-edge/vite';
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
@@ -5,7 +6,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), vercelEdgeAdapter({
+      ssg: {
+        include: ['/*'],
+        origin: 'https://broky.dev',
+        sitemapOutFile: 'sitemap.xml',
+      },
+    })],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
